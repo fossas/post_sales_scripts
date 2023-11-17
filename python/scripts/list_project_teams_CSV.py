f7830b9fa43d4341e0cf5bbd37631f027b552da8
Ryan Link <47920994+ryanlink@users.noreply.github.com>
2023-11-17 12:49:27 -0600

    Rewrite to use /v2/projects endpoint

--- python/scripts/list_project_teams_CSV.py
+++ python/scripts/list_project_teams_CSV.py
@@ -1,23 +1,39 @@
 import os
 import requests
+import sys
 
 # Check if FOSSA_API_KEY is set
 fossa_api_key = os.environ.get('FOSSA_API_KEY')
 if not fossa_api_key:
     exit("FOSSA_API_KEY is not set. Please set the environment variable.")
 
-# Set FOSSA API base URL
-fossa_api_base_url = "https://app.fossa.com/api"
+# Set the FOSSA API base URL
+v2_api_base_url = "https://app.fossa.com/api/v2"
 fossa_api_key_header = {"Authorization": f"Bearer {fossa_api_key}"}
 
-# Get all projects
-projects = requests.get(f"{fossa_api_base_url}/projects", headers=fossa_api_key_header).json()
+# Function to get team names for a project
+def get_team_names(project):
+    teams = project.get('teams', [])
+    team_names = [team['name'] for team in teams]
+    return ",".join(team_names)
 
-# Create and print CSV header
+# Get all projects from v2 endpoint with pagination
+page = 1
+projects_data = []
+while True:
+    response = requests.get(f"{v2_api_base_url}/projects?count=1000&page={page}", headers=fossa_api_key_header)
+    response.raise_for_status()
+    page_data = response.json().get('projects', [])
+    if not page_data:
+        break
+    projects_data.extend(page_data)
+    page += 1
+
+# Print CSV header
 print("ProjectTitle,TeamNames")
 
-# Loop through projects and print their details
-for p in projects:
-    title = p.get('title')
-    teams = [requests.get(f"{fossa_api_base_url}/teams/{t['teamId']}", headers=fossa_api_key_header).json().get('name') for t in p.get('teamProjects', [])]
-    print(f"{title},{','.join(teams)}")
+# Loop through each project and print project details
+for project in projects_data:
+    project_title = project.get('title', 'Unknown Project')
+    team_names = get_team_names(project)
+    print(f"{project_title},{team_names}")
